23f7ddfa17e6fc8df1af05fb101e8c8928481c91
Kyle Uy <kyleeeee09@gmail.com>
2023-07-15 13:03:01 +0800

    Added checkbox validation. Checks if user selected a bike or not.

--- OrderingSystem/index.js
+++ OrderingSystem/index.js
@@ -1,56 +1,38 @@
-<<<<<<< HEAD
-document.getElementById('bikeForm').addEventListener('submit', function(event) {
+document.getElementById('bikeForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  var isChecked = false;
+
+  var isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+
+  if (!isChecked) {
     event.preventDefault();
-  
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    
-    var totalPrice = 0;
-    var itemSummary = document.getElementById('item-summary');
-  
-    itemSummary.innerHTML = "";
-  
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) {
-        totalPrice += Number(checkboxes[i].dataset.price);
-        
-        var li = document.createElement('li');
-        li.className = "list-group-item";
-        li.innerHTML = checkboxes[i].nextElementSibling.innerText + " - $" + checkboxes[i].dataset.price;
-        itemSummary.appendChild(li);
-      }
+    alert('Please select atleast one bike.')
+    return;
+  }
+
+
+  var totalPrice = 0;
+  var itemSummary = document.getElementById('item-summary');
+
+  itemSummary.innerHTML = "";
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      totalPrice += Number(checkboxes[i].dataset.price);
+
+      var li = document.createElement('li');
+      li.className = "list-group-item";
+      li.innerHTML = checkboxes[i].nextElementSibling.innerText + " - $" + checkboxes[i].dataset.price;
+      itemSummary.appendChild(li);
     }
-  
-    var summaryCard = document.getElementById('summary-card');
-    summaryCard.classList.remove('d-none');
-  
-    document.getElementById('total-price').innerText = "Total Price: $" + totalPrice;
-  });
-=======
-document.getElementById('bikeForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    
-    var totalPrice = 0;
-    var itemSummary = document.getElementById('item-summary');
-  
-    itemSummary.innerHTML = "";
-  
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) {
-        totalPrice += Number(checkboxes[i].dataset.price);
-        
-        var li = document.createElement('li');
-        li.className = "list-group-item";
-        li.innerHTML = checkboxes[i].nextElementSibling.innerText + " - $" + checkboxes[i].dataset.price;
-        itemSummary.appendChild(li);
-      }
-    }
-  
-    var summaryCard = document.getElementById('summary-card');
-    summaryCard.classList.remove('d-none');
-  
-    document.getElementById('total-price').innerText = "Total Price: $" + totalPrice;
-  });
->>>>>>> 38d35f5 (Uploaded two new web pages.)
-  
+  }
+
+  var summaryCard = document.getElementById('summary-card');
+  summaryCard.classList.remove('d-none');
+
+  document.getElementById('total-price').innerText = "Total Price: $" + totalPrice;
+});
+
