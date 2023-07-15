6d3de772ee1032471fdae80c054fc2c704432e85
Kyle Uy <kyleeeee09@gmail.com>
2023-07-15 12:26:20 +0800

    Uploaded two new web pages.

--- OrderingSystem/index.js
+++ OrderingSystem/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById('bikeForm').addEventListener('submit', function(event) {
     event.preventDefault();
   
@@ -24,4 +25,32 @@ document.getElementById('bikeForm').addEventListener('submit', function(event) {
   
     document.getElementById('total-price').innerText = "Total Price: $" + totalPrice;
   });
+=======
+document.getElementById('bikeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    var totalPrice = 0;
+    var itemSummary = document.getElementById('item-summary');
+  
+    itemSummary.innerHTML = "";
+  
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        totalPrice += Number(checkboxes[i].dataset.price);
+        
+        var li = document.createElement('li');
+        li.className = "list-group-item";
+        li.innerHTML = checkboxes[i].nextElementSibling.innerText + " - $" + checkboxes[i].dataset.price;
+        itemSummary.appendChild(li);
+      }
+    }
+  
+    var summaryCard = document.getElementById('summary-card');
+    summaryCard.classList.remove('d-none');
+  
+    document.getElementById('total-price').innerText = "Total Price: $" + totalPrice;
+  });
+>>>>>>> 38d35f5 (Uploaded two new web pages.)
   
